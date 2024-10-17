8d5f8d9e260575fd6505b584f68a231bbb2fdc78
kareemrogers-coder <kareemrogers27@gmail.com>
2024-10-17 00:54:36 -0400

    stacks and queues

--- Order.py
+++
@@ -1,7 +0,0 @@
-class Order:
-
-    def __init__(self, meals, table_number):
-        self.meals = meals
-        self.table_number= table_number
-        self.next = None
-    
--- kitchen.py
+++ kitchen.py
@@ -1,9 +1,10 @@
 from helper import default_message
 
-class Node:
+class Order:
 
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, meals, table_number):
+        self.meals = meals
+        self.table_number= table_number
         self.next = None
 
 class Kitchen:
@@ -12,8 +13,8 @@ class Kitchen:
         self.head = None
         self.tail = None
 
-    def enqueue(self, order):
-        new_node = Node(order)
+    def enqueue(self, meals, table_number):
+        new_node = Order(meals, table_number)
         if self.head == None:
             self.head = new_node
             self.tail = new_node
@@ -24,7 +25,7 @@ class Kitchen:
     def traverse(self):
         current = self.head
         while current:
-            print(current.data)
+            print(f"{current.meals} for {current.table_number}")
             current =current.next
 
     def dequeue(self):
@@ -33,16 +34,16 @@ class Kitchen:
         else:
             removed = self.head
             self.head =removed.next
-            print(f"{removed.data} is being cook")
+            print(f"{removed.meals} for {removed.table_number} is being cook")
 
     
 orders = Kitchen()
 
 default_message()
-orders.enqueue("Hotdog")
-orders.enqueue("Red snapper fish and Rice")   
-orders.enqueue("Hamburger")
-orders.enqueue("12pc wings")
+orders.enqueue("Hotdog","Table: 1")
+orders.enqueue("Red snapper fish and Rice","Table: 2")   
+orders.enqueue("Hamburger","Table: 3")
+orders.enqueue("12pc wings","Table: 4")
 
 orders.traverse()
 
